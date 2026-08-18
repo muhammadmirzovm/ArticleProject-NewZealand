af9ecadf0ab8302f8f32bcdc722ced3d78e5a9c6
mukhammadmirzo <mukhammadmirzo078@gmail.com>
2026-08-18 16:25:48 +0500

    added article delete , edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
    path("", views.article_list, name="article_list"),
    path("articles/<slug:slug>/",  views.article_detail, name="article_detail"),
    path("article/create/", views.article_create, name="article_create"),
+   path("articles/<slug:slug>/edit/", views.article_edit, name="article_edit"),
+   path("articles/<slug:slug>/delete/", views.article_delete, name="article_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Article
 from django.contrib.auth.decorators import login_required
 from .forms import ArticleForm
+from django.http import HttpResponseForbidden
 def article_list(request):
    articles = Article.objects.all().order_by("-created_at")
    return render(request, "blog/article_list.html", {"articles": articles})
@@ -12,10 +13,36 @@ def article_detail(request, slug):
 
 @login_required
 def article_create(request):
-   form = ArticleForm(request.POST or None, request.FILES or None)
+      form = ArticleForm(request.POST or None, request.FILES or None)
+      if request.method == "POST" and form.is_valid():
+         article = form.save(commit=False)
+         article.author = request.user
+         article.save()
+         return redirect("article_detail", slug=article.slug)
+      return render(request, "blog/article_form.html", {"form": form})
+#from django.http import HttpResponseForbidden
+@login_required
+def article_edit(request, slug):
+   article = get_object_or_404(Article, slug=slug)
+   if article.author != request.user:
+       return HttpResponseForbidden("Ruxsat yo‘q")
+   form = ArticleForm(request.POST or None, request.FILES or None, instance=article)
    if request.method == "POST" and form.is_valid():
-       article = form.save(commit=False)
-       article.author = request.user
-       article.save()
+       form.save()
        return redirect("article_detail", slug=article.slug)
    return render(request, "blog/article_form.html", {"form": form})
+
+
+@login_required
+def article_delete(request, slug):
+   article = get_object_or_404(Article, slug=slug)
+
+   if article.author != request.user:
+       return HttpResponseForbidden("Ruxsat yo‘q")
+
+   if request.method == "POST":
+       article.delete()
+       return redirect("article_list")
+   return render(request, "blog/article_confirm_delete.html", {"article": article})
+
+
